ce4f21cad3c6a335b9c7a769d1e94f3f0c9f2f6e
liyi <cfylee42@gmail.com>
2026-07-16 19:25:46 +0800

    feat: green – #6 – Implement momentum features (FR-0100)
    Closes #6

---
+++ src/trader_off/features/__init__.py
@@ -0,0 +1,7 @@
+"""Feature engineering: momentum, volatility, volume indicators."""
+
+from trader_off.features.momentum import compute_momentum_features
+
+__all__ = [
+    "compute_momentum_features",
+]
